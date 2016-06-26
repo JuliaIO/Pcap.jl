1a7d136c8460f7361e886f05f0d52ce7a39bf792
Brandon Miller <brandonkentmiller@gmail.com>
2016-06-26 17:16:19 -0400

    starting live capture dev

---
+++ deps/src/live_cap.c
@@ -0,0 +1,16 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  live_cap.c
+ *
+ *    Description:  wrappers for libpcap live capture functionality
+ *
+ *        Created:  06/26/2016 05:06:29 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Brandon K. Miller (bkm), brandonkentmiller@gmail.com 
+ *
+ * =====================================================================================
+ */
+
+#include "live_cap.h"
---
+++ deps/src/live_cap.h
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  live_cap.h
+ *
+ *    Description:  includes for live_cap.c
+ *
+ *        Created:  06/26/2016 05:09:30 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Brandon K. Miller (bkm), brandonkentmiller@gmail.com 
+ *
+ * =====================================================================================
+ */
+
+#ifndef _LIVE_CAP_H
+#define _LIVE_CAP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif
