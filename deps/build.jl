913c8f5720dcbc55c66e6909fa4c29526354d8f9
Brandon Miller <brandonkentmiller@gmail.com>
2016-07-01 18:32:52 -0400

    got deps.jl generating finally

--- deps/build.jl
+++ deps/build.jl
@@ -2,26 +2,26 @@ using BinDeps
 
 @BinDeps.setup
 
-deps = [ pcap = library_dependency("Pcap", aliases = ["libpcap"]) ]
+jlpcap = library_dependency("libjlpcap")
 
 @linux_only begin
-    prefix = joinpath(BinDeps.depsdir(pcap), "usr")
-    pcapsrcdir = joinpath(BinDeps.depsdir(pcap), "src", "pcap-lib")
-    pcapbuilddir = joinpath(BinDeps.depsdir(pcap), "builds", "pcap-lib")
+    prefix = joinpath(BinDeps.depsdir(jlpcap), "usr")
+    pcapsrcdir = joinpath(BinDeps.depsdir(jlpcap), "src", "pcap-lib")
+    pcapbuilddir = joinpath(BinDeps.depsdir(jlpcap), "builds", "pcap-lib")
     provides(BuildProcess,
             (@build_steps begin
                 CreateDirectory(pcapbuilddir)
                 CreateDirectory("$prefix/lib")
                 @build_steps begin
                     ChangeDirectory(pcapbuilddir)
-                    FileRule(joinpath(prefix, "lib", "libpcap.so"), @build_steps begin
+                    FileRule(joinpath(prefix, "lib", "libjlpcap.so"), @build_steps begin
                         `gcc -c -O -W -Wall -fpic -std=gnu99 $pcapsrcdir/pcap-lib.c -lpcap`
-                        `gcc -shared -o libpcap.so pcap-lib.o`
-                        `cp libpcap.so $prefix/lib`
+                        `gcc -shared -o libjlpcap.so pcap-lib.o`
+                        `cp libjlpcap.so $prefix/lib`
                         `cp $pcapsrcdir/pcap-lib.h $prefix/include`
                     end)
                 end
-            end), pcap, os = :Linux)
+            end), jlpcap, os = :Linux)
 end
 
-@BinDeps.install
+@BinDeps.install Dict(:libjlpcap => :libjlpcap)
--- src/online.jl
+++ src/online.jl
@@ -1,12 +1,26 @@
-export pcap_lookupdev
+import GetC.@getCFun
 
-#----------
-# lookup default device
-#----------
-function pcap_lookupdev()
-    dev = ccall((:pcap_lookupdev, "libpcap"), Ptr{UInt8}, ())
-    if dev == C_NULL
-        return Union()
-    end
-    bytestring(dev)
-end # function pcap_lookupdev
+if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
+    include("../deps/deps.jl")
+else
+    error("Pcap not properly installed. Please run Pkg.build(\"Pcap\")")
+end
+
+const pcaplib = "libjlpcap"
+
+@getCFun pcaplib pcap_open_live capture_open_live(
+    device::Ptr{UInt8}, snaplen::Int32, promisc::Int32, ms::Int32
+)::Int32
+export pcap_open_live
+
+@getCFun pcaplib pcap_set_filter capture_set_filter(filter::Ptr{UInt8})::Int32
+export pcap_set_filter
+
+@getCFun pcaplib pcap_close capture_close()::Void
+export pcap_close
+
+@getCFun pcaplib pcap_loop capture_loop()::Int32
+export pcap_loop
+
+
+
