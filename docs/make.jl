eabfc398acf7ac57a333a6515c690825affa674a
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-03-04 12:52:47 +0530

    Fix URLs and package name

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
-using MakiePango
+using Pcap
 using Documenter
 
-DocMeta.setdocmeta!(MakiePango, :DocTestSetup, :(using MakiePango); recursive=true)
+DocMeta.setdocmeta!(Pcap, :DocTestSetup, :(using Pcap); recursive=true)
 
 makedocs(;
-    modules=[MakiePango],
-    authors="Anshul Singhvi <anshulsinghvi@gmail.com> and contributors",
-    repo="https://github.com/MakieOrg/MakiePango.jl/blob/{commit}{path}#{line}",
-    sitename="MakiePango.jl",
+    modules=[Pcap],
+    authors="Pcap.jl contributors",
+    repo="https://github.com/JuliaIO/Pcap.jl/blob/{commit}{path}#{line}",
+    sitename="Pcap.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://MakieOrg.github.io/MakiePango.jl",
+        canonical="https://JuliaIO.github.io/Pcap.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -20,6 +20,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/MakieOrg/MakiePango.jl",
+    repo="github.com/JuliaIO/Pcap.jl",
     devbranch="main",
 )
